8bea41785b5001965ce0a237b0b85a376ab8f9ba
Pramot Sereesuksan_Apar <pramot.sere@kkpfg.com>
2022-12-06 14:12:07 +0700

    Edit Logo Application

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,24 +1,20 @@
 // import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../../assets/images/kkp_logo.png'
-
+import Logo from '../../assets/icons/Kaitnakin-Phatra-Bank-Logo.png'
+// import Logo from '../../assets/icons/kkp_logo.png'
 function Navbars() {
   return (
     <>
-      <Navbar bg="light" variant="light">
-        {/* <Container> */}
+      <Navbar bg="light" variant="light" style={{maxHeight: 50}} >
           <Navbar.Brand>
             <img
               alt=""
               src={Logo}
-              // width="30"
-              height="30"
+              height="60"
               className="d-inline-block align-top"
             />
             {' '}
-            {/* Decision Config */}
           </Navbar.Brand>
-        {/* </Container> */}
       </Navbar>
     </>
   );
