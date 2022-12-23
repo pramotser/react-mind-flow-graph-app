269fda109b3a37f516ba025b17a694d3e2089359
Pramot Sereesuksan_Apar <pramot.sere@kkpfg.com>
2022-12-23 13:58:45 +0700

    Custom react-datepicker select time

--- src/components/datepicker/Datepicker.jsx
+++ src/components/datepicker/Datepicker.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import './datepicker.scss'
+import React, { useState } from 'react'
 
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
 import { Form } from 'react-bootstrap'
 
 const DatepickerCustom = (props) => {
+    // const CustomInputTime = ({ value, onChange }) => (
+    //     <>
+    //         <Form.Control
+    //             type="time"
+    //             value={value}
+    //             onChange={(e) => onChange(e.target.value)}
+    //         />
+    //     </>
+    // );
     return (
         <>
             <DatePicker
@@ -14,16 +26,27 @@ const DatepickerCustom = (props) => {
                 minDate={props.minDate}
                 onChange={(date) => props.onChange(date)}
                 placeholderText={props.placeholderText}
+                customInput={(<Form.Control
+                    type="text"
+                    name='datepicker'
+                    required={props.required}
+                />)}
                 required={props.required}
-                showTimeInput={props.showTimeInput}
                 disabled={props.disabled}
-                customInput={(
-                    <Form.Control
-                        type="text"
-                        name='datepicker'
-                        required={props.required}
-                    />
-                )}
+                // maxTime={new Date().setHours(23, 59, 59)}
+                // minTime={new Date().setHours(0, 0)}
+                // showTimeInput={props.showTimeInput}
+                // timeInputLabel="Time : "
+                // customTimeInput={<CustomInputTime />}
+                showTimeSelect={true}
+                timeFormat="HH:mm"
+                injectTimes={[
+                    setHours(setMinutes(new Date(), 1), 0),
+                    setHours(setMinutes(new Date(), 5), 12),
+                    setHours(setMinutes(new Date(), 59), 23),
+                ]}
+                showMonthDropdown
+                showYearDropdown
             />
         </>
     )
